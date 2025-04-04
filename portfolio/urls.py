20a5514b25d3574e130e860cb80606fee810bbc2
Duy Tran <duytran2310@gmail.com>
2025-04-04 03:19:37 -0500

    added other tabs

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import HomePageView
+from .views import HomePageView, ArticleView, ProjectView, VideoView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('get_videos', VideoView.as_view(), name='get_videos'),
+    path('get_articles', ArticleView.as_view(), name='get_articles'),
+    path('get_projects', ProjectView.as_view(), name='get_projects')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -7,3 +7,20 @@ class HomePageView(ListView):
     model = Blog
     context_object_name = 'blogs'
     template_name = 'home.html'
+
+class BlogView(ListView):
+    model = Blog
+    context_object_name = 'blogs'
+    template_name = 'blog.html'
+
+class ArticleView(BlogView):
+    def get_queryset(self):
+        return Blog.objects.filter(category='article')
+
+class ProjectView(BlogView):
+    def get_queryset(self):
+        return Blog.objects.filter(category='app')
+
+class VideoView(BlogView):
+    def get_queryset(self):
+        return Blog.objects.filter(category='video')
