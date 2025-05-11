3a938664f39bb116322f06aa84a52e2fb8e12f57
Duy Tran <duytran2310@gmail.com>
2025-05-11 16:10:36 -0500

    refactored

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import AboutView, TutorialView, ProjectView, get_screenshot
+from .views import AboutView, TutorialView, ProjectView
 
 urlpatterns = [
     path('', ProjectView.as_view(), name='home'),
     path('about', AboutView.as_view(), name='about'),
     path('get_tutorials', TutorialView.as_view(), name='get_tutorials'),
-    path('get_projects', ProjectView.as_view(), name='get_projects'),
-    path('<int:blog_pk>/get_screenshot', get_screenshot, name='get_screenshot')
+    path('get_projects', ProjectView.as_view(), name='get_projects')
 ]
